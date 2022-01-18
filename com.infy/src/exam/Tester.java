badfb320d13b661d9549bfc05984ae44aae0574a
RUMANA <RUMANA@Rumana>
2022-01-18 20:11:12 +0530

    commited

---
+++ com.infy/src/exam/Tester.java
@@ -0,0 +1,10 @@
+package exam;
+
+public class Tester {
+	public static void main(String[] args) {
+		System.out.println("Welcome Rumana,how an i help you...");
+		System.out.println("Thank you lappy");
+	}
+	}
+
+
