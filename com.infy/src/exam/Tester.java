828d55c8864d1a7e561a2ea115d870604e4fa0f0
RUMANA <RUMANA@Rumana>
2022-01-18 20:51:43 +0530

    second commit

--- com.infy/src/exam/Tester.java
+++ com.infy/src/exam/Tester.java
@@ -3,7 +3,7 @@ package exam;
 public class Tester {
 	public static void main(String[] args) {
 		System.out.println("Welcome Rumana,how an i help you...");
-		System.out.println("Thank you lappy");
+		System.out.println("Thank you lappy");     
 	}
 	}
 
